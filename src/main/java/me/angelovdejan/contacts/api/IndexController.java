0adab20b23123d6deef89186fea823e124b8414a
Dejan Angelov <angelovdejan92@gmail.com>
2019-01-19 16:56:54 +0100

    swagger docs init

--- src/main/java/me/angelovdejan/contacts/api/IndexController.java
+++ src/main/java/me/angelovdejan/contacts/api/IndexController.java
@@ -1,6 +1,7 @@
 package me.angelovdejan.contacts.api;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
@@ -16,5 +17,10 @@ public class IndexController {
         output.put("message", "Hello world!");
 
         return output;
+    }
+
+    @GetMapping(path = {"hello/{name}"})
+    public String hello(@PathVariable String name) {
+        return "Hello " + name;
     }
 }
